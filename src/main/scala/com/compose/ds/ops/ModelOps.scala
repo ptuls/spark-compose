09f5610e5fdc4fb39dc189403ba5a3d0374e3731
Paul Tune <paul.t@canva.com>
2018-02-04 20:10:29 +1100

    Updated free monad definitions

--- src/main/scala/com/compose/ds/ops/ModelOps.scala
+++ src/main/scala/com/compose/ds/ops/ModelOps.scala
@@ -1,20 +1,29 @@
 package com.compose.ds.ops
 
-import com.compose.ds.ops.Context.DataSet
+import com.compose.ds.ops.Context.{DataSet, State}
 import org.apache.spark.sql.Dataset
 
 object Context {
   type DataSet[A] = Dataset[A]
-  type Weights[A] = Array[A]
+  type State[A] = Dataset[A]
 }
 
+/**
+  *
+  * Definition:
+  * 1. in fitting, a model takes in a dataset and produces a model state
+  * 2. in transforming, a model takes a dataset and a model state and produces a dataset
+  *
+  */
 sealed trait ModelF[A]
 object Model {
   case class Fit[A](dataset: DataSet[_]) extends ModelF[A]
-  case class Load[A](weights: Array[_]) extends ModelF[A]
-  case class Transform[A](dataset: DataSet[_]) extends ModelF[A]
+  case class Transform[A](dataset: DataSet[_], state: State[A]) extends ModelF[A]
 }
 
+/**
+  * A transformer is simply a pure function that takes in a dataset and produces a dataset.
+  */
 sealed trait TransformerF[A]
 object Transformer {
   case class Transform[A](dataset: DataSet[_]) extends TransformerF[A]
